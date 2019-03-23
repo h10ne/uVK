bcb9c240eca9bf54c8ea1dd547d80093438c3587
dr0b99 <48856319+dr0b99@users.noreply.github.com>
2019-03-23 21:40:53 +0300

    Add files via upload

---
+++ uVK/uVK/ViewModel/Base/BaseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace uVK
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
